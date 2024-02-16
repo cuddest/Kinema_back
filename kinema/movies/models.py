69df9963a2558d1761b2aec22db2b6609043fb5f
cuddest <142700499+cuddest@users.noreply.github.com>
2024-02-16 13:52:34 +0100

    nc

---
+++ kinema/movies/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
+
+class Movie(models.Model):
+    Title = models.CharField(max_length=100)
+    Release_Date = models.DateField()
+    Genre = models.CharField(max_length=100)
+    Language = models.CharField(max_length=100)
+    Duration = models.CharField(max_length=100)
+    Director = models.CharField(max_length=100)
+    Cast = models.CharField(max_length=100)
+    Description = models.TextField()
+    Poster = models.ImageField(upload_to="movie_posters", blank=True, null=True)
+
+    def __str__(self):
+        return self.Title
+
+
+# Create your models here.
